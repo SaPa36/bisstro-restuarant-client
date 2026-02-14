f15f34d2267dbb5ecca627bfa1e948fb73b52a9a
Sapa <nursapa2001@gmail.com>
2026-02-14 22:40:52 +0600

    setup routes and footer

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Main from "../Layout/Main";
+import Home from "../pages/Home/Home";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children:[
+        {
+            path: '/',
+            element: <Home></Home>
+        }
+    ]
+  },
+]);
+
+const Routes = () => {
+  return <div></div>;
+};
+
+export default Routes;
