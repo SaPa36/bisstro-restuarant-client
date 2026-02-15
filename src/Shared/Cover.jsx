66a6997ba122c7725208e8aee6f433d52bb0fb3a
SaPa36 <nursapa2001@gmail.com>
2026-02-15 17:10:04 +0600

    use parrallax

--- src/Shared/Cover.jsx
+++ src/Shared/Cover.jsx
@@ -1,26 +1,32 @@
-import React from 'react';
+import { Parallax, Background } from 'react-parallax';
 
-const Cover = ({img, title}) => {
+const Cover = ({ img, title }) => {
     return (
-        <div
-            className="hero h-[500px] rounded-lg"
-            style={{
-                backgroundImage:
-                    `url(${img})`,
-            }}
+
+        <Parallax
+            blur={{ min: -15, max: 15 }}
+            bgImage={img}
+            bgImageAlt="the menu"
+            strength={-200}
         >
-            <div className="hero-overlay"></div>
-            <div className="hero-content text-neutral-content text-center">
-                <div className="max-w-md">
-                    <h1 className="mb-5 text-5xl font-bold">{title}</h1>
-                    <p className="mb-5">
-                        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-                        quasi. In deleniti eaque aut repudiandae et a id nisi.
-                    </p>
-                    
+            <div
+                className="hero h-[500px] rounded-lg"
+            >
+                <div className="hero-overlay"></div>
+                <div className="hero-content text-neutral-content text-center">
+                    <div className="max-w-md">
+                        <h1 className="mb-5 text-5xl font-bold">{title}</h1>
+                        <p className="mb-5">
+                            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+                            quasi. In deleniti eaque aut repudiandae et a id nisi.
+                        </p>
+
+                    </div>
                 </div>
             </div>
-        </div>
+        </Parallax>
+
+
     );
 };
 
