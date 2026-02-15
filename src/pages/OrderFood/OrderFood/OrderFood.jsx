38293bfcff77679c4c233ade02350f5bca0a2bad
SaPa36 <nursapa2001@gmail.com>
2026-02-15 18:23:27 +0600

    create order food cover

--- src/pages/OrderFood/OrderFood/OrderFood.jsx
+++ src/pages/OrderFood/OrderFood/OrderFood.jsx
@@ -3,8 +3,10 @@ import Cover from '../../../Shared/Cover';
 import orderImg from '../../../assets/shop/banner2.jpg';
 
 const OrderFood = () => {
+    // order food page will be implemented later. For now, just showing the cover image.
     return (
         <div>
+            
             <Cover img={orderImg} title="Order Food"></Cover>
         </div>
     );
