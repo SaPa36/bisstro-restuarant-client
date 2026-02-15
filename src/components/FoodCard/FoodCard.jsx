0bd594a4ad78813bc56199a7a85998bde3d75a85
Sapa <nursapa2001@gmail.com>
2026-02-15 23:12:39 +0600

    create tab and shows data

---
+++ src/components/FoodCard/FoodCard.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+const FoodCard = ({item}) => {
+
+    const {image, name, recipe, price} = item;
+  return (
+    <div className="card bg-[#F3F3F3] shadow-xl rounded-none">
+      <figure>
+        <img
+          src={image}
+          alt="Caeser Salad"
+          className="w-full h-[280px] object-cover" // Shorter image height
+        />
+      </figure>
+      <p className="absolute right-0 mr-4 mt-4 bg-slate-900 px-4 text-white">{price}</p>
+      <div className="card-body items-center text-center p-6">
+        {" "}
+        {/* Reduced padding */}
+        <h2 className="card-title text-xl font-bold">{name}</h2>
+        <p className="text-sm text-[#151515]">
+          {recipe}
+        </p>
+        <div className="card-actions mt-4">
+          <button className="btn btn-outline border-0 border-b-4 border-[#BB8506] bg-[#E8E8E8] text-[#BB8506] uppercase hover:bg-[#111827] hover:border-[#BB8506]">
+            Add to Cart
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FoodCard;
--- src/pages/OrderFood/OrderFood/OrderFood.jsx
+++ src/pages/OrderFood/OrderFood/OrderFood.jsx
@@ -1,15 +1,50 @@
-import React from 'react';
-import Cover from '../../../Shared/Cover';
-import orderImg from '../../../assets/shop/banner2.jpg';
+import React, { useState } from "react";
+import Cover from "../../../Shared/Cover";
+import orderImg from "../../../assets/shop/banner2.jpg";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
+import FoodCard from "../../../components/FoodCard/FoodCard";
+import useMenu from "../../../hooks/useMenu";
 
 const OrderFood = () => {
-    // order food page will be implemented later. For now, just showing the cover image.
-    return (
-        <div>
-            
-            <Cover img={orderImg} title="Order Food"></Cover>
-        </div>
-    );
+    const [tabIndex, setTabIndex] = useState(0);
+    const [menu] = useMenu();
+
+    const offered = menu.filter(item => item.category === 'offered');
+    const dessert = menu.filter(item => item.category === 'dessert');
+    const pizza = menu.filter(item => item.category === 'pizza');
+    const salad = menu.filter(item => item.category === 'salad');
+    const soup = menu.filter(item => item.category === 'soup');
+  return (
+    <div>
+      <Cover img={orderImg} title="Order Food"></Cover>
+      <Tabs defaultIndex={tabIndex} onSelect={(index) => setTabIndex(index)}>
+        <TabList>
+          <Tab>Salads</Tab>
+          <Tab>Pizza</Tab>
+          <Tab>Soups</Tab>
+          <Tab>Desserts</Tab>
+          <Tab>Drinks</Tab>
+        </TabList>
+        <TabPanel>
+            <div className="grid md:grid-cols-3 gap-10">
+                {
+                    salad.map(item => <FoodCard
+                        key={item._id}
+                        item={item}
+                    >
+
+                    </FoodCard>)
+                }
+            </div>
+        </TabPanel>
+        <TabPanel></TabPanel>
+        <TabPanel></TabPanel>
+        <TabPanel></TabPanel>
+        <TabPanel></TabPanel>
+      </Tabs>
+    </div>
+  );
 };
 
 export default OrderFood;
