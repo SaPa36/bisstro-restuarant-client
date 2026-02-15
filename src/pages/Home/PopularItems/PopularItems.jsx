b99cbdb66c46280ffc810a65f8f522aa1986bbc6
SaPa36 <nursapa2001@gmail.com>
2026-02-15 17:39:37 +0600

    create a custom hook to show category wise menu

--- src/pages/Home/PopularItems/PopularItems.jsx
+++ src/pages/Home/PopularItems/PopularItems.jsx
@@ -2,25 +2,27 @@ import React, { useEffect, useState } from 'react';
 
 import MenuItem from '../../../Shared/MenuItem';
 import SectionTitle from '../../../components/SectionTitle/SectionTitle';
+import useMenu from '../../../hooks/useMenu';
 
 const PopularItems = () => {
 
-    const [menu, setMenu] = useState([]);
+    const [menu] = useMenu();
+    const popularItems = menu.filter(item => item.category === 'popular');
 
-    useEffect(() => {
-        fetch('menu.json')
-            .then(res => res.json())
-            .then(data => {
-                const PopularItems = data.filter(item => item.category === 'popular');
-                setMenu(PopularItems);
-            })
-    }, []);
+    // useEffect(() => {
+    //     fetch('menu.json')
+    //         .then(res => res.json())
+    //         .then(data => {
+    //             const PopularItems = data.filter(item => item.category === 'popular');
+    //             setMenu(PopularItems);
+    //         })
+    // }, []);
 
     return (
         <section className='mb-12'>
             <SectionTitle heading="From Our Menu" subHeading="Popular Items"></SectionTitle>
             <div className='grid md:grid-cols-2 gap-10'>
-                {menu.map(item =>
+                {popularItems.map(item =>
                     <MenuItem
                         key={item._id}
                         item={item}>
