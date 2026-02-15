a7295c1a02e4147909ad29e4ee18eeb57a7f4d53
SaPa36 <nursapa2001@gmail.com>
2026-02-15 11:48:48 +0600

    create category part

---
+++ src/pages/Home/Category/Category.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+// import required modules
+import { Pagination } from 'swiper/modules';
+
+import slide1 from '../../../assets/home/slide1.jpg'
+import slide2 from '../../../assets/home/slide2.jpg'
+import slide3 from '../../../assets/home/slide3.jpg'
+import slide4 from '../../../assets/home/slide4.jpg'
+import slide5 from '../../../assets/home/slide5.jpg'
+
+const Category = () => {
+    return (
+        <Swiper
+            slidesPerView={4}
+            spaceBetween={30}
+            centeredSlides={true}
+            pagination={{
+                clickable: true,
+            }}
+            modules={[Pagination]}
+            className="mySwiper mb-20"
+        >
+            <SwiperSlide><img src={slide1} alt="Slide 1" />
+                <h3 className='uppercase text-center -mt-16 text-white text-3xl'>Salads</h3>
+            </SwiperSlide>
+            <SwiperSlide><img src={slide2} alt="Slide 2" />
+                <h3 className='uppercase text-center -mt-16 text-white text-3xl'>Pizzas</h3>
+            </SwiperSlide>
+            <SwiperSlide><img src={slide3} alt="Slide 3" />
+                <h3 className='uppercase text-center -mt-16 text-white text-3xl'>Soups</h3>
+            </SwiperSlide>
+            <SwiperSlide><img src={slide4} alt="Slide 4" />
+                <h3 className='uppercase text-center -mt-16 text-white text-3xl'>Desserts</h3>
+            </SwiperSlide>
+            <SwiperSlide><img src={slide5} alt="Slide 5" />
+                <h3 className='uppercase text-center -mt-16 text-white text-3xl'>Salads</h3>
+            </SwiperSlide>
+            
+        </Swiper>
+    );
+};
+
+export default Category;
--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
+import Category from '../Category/Category';
 
 const Home = () => {
     return (
-        <div>
+        <div className='space-y-20'>
             <Banner></Banner>
+            <Category></Category>
         </div>
     );
 };
