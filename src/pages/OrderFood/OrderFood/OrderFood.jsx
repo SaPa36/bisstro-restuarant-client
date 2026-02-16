af19f54e68a7409c4bff296863faa9bd7bed29b3
SaPa36 <nursapa2001@gmail.com>
2026-02-16 11:29:01 +0600

    apply pagination and load data from server

--- src/pages/OrderFood/OrderFood/OrderFood.jsx
+++ src/pages/OrderFood/OrderFood/OrderFood.jsx
@@ -5,11 +5,16 @@ import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import "react-tabs/style/react-tabs.css";
 import useMenu from "../../../hooks/useMenu";
 import OrderTab from "../OrderTab/OrderTab";
+import { useParams } from "react-router-dom";
 
 const OrderFood = () => {
-  const [tabIndex, setTabIndex] = useState(0);
   const [menu] = useMenu();
-
+  const categories = ["salad", "pizza", "soup", "dessert", "drinks"];
+  const { category } = useParams();
+  const initialIndex = categories.indexOf(category);
+  const [tabIndex, setTabIndex] = useState(initialIndex);
+  
+  // Filter menu items based on category
   const drinks = menu.filter((item) => item.category === "drinks");
   const dessert = menu.filter((item) => item.category === "dessert");
   const pizza = menu.filter((item) => item.category === "pizza");
--- src/pages/OrderFood/OrderTab/OrderTab.jsx
+++ src/pages/OrderFood/OrderTab/OrderTab.jsx
@@ -1,13 +1,42 @@
-
 import FoodCard from "../../../components/FoodCard/FoodCard";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { Pagination } from 'swiper/modules';
 
-const OrderTab = ({items}) => {
+const OrderTab = ({ items }) => {
+  // 1. Logic to chunk the items into groups of 6
+  const itemsPerPage = 6;
+  const pages = [];
+  
+  for (let i = 0; i < items.length; i += itemsPerPage) {
+    pages.push(items.slice(i, i + itemsPerPage));
+  }
+
+  const pagination = {
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + (index + 1) + '</span>';
+    },
+  };
+
   return (
-    <div className="grid md:grid-cols-3 gap-10">
-      {items.map((item) => (
-        <FoodCard key={item._id} item={item}></FoodCard>
+    <Swiper
+      pagination={pagination}
+      modules={[Pagination]}
+      className="mySwiper"
+    >
+      {/* 2. Map through the 'pages' array to create a slide for every 6 items */}
+      {pages.map((group, index) => (
+        <SwiperSlide key={index}>
+          <div className="grid md:grid-cols-3 gap-10 mb-12">
+            {group.map((item) => (
+              <FoodCard key={item._id} item={item}></FoodCard>
+            ))}
+          </div>
+        </SwiperSlide>
       ))}
-    </div>
+    </Swiper>
   );
 };
 
